e726782c7c114a4fb20028b499e966312c849b91
Cong1802 <89115822+Cong1802@users.noreply.github.com>
2021-09-03 20:03:11 +0700

    upload

---
+++ js/Untitled-3.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+$(".login_admin2").on("click",function(){
+	var account = $(".name").val();
+	var password = $(".pass").val();
+	$(".hello").remove();
+	$(".hello2").remove();
+	if(account=="")
+		{
+			$(".name").parent().append("<li class='hello' style='color:red'>Vui lòng nhập tài khoản</li>");
+			$(".name").css("border-color","red")
+		}
+	else
+		{
+			$(".hello").remove();
+			$(".name").css("border-color","black")
+		}
+	if(password=="")
+		{
+			$(".pass").parent().append("<li class='hello2' style='color:red'>Vui lòng nhập mật khẩu</li>");
+			$(".pass").css("border-color","red")
+		}
+	else
+		{
+			$(".hello2").remove();
+			$(".pass").css("border-color","black")
+		}
+})
+$(document).ready(function() {
+	$(".eye").click(function(){
+		$(this).toggleClass("open");
+		$(this).children("i").toggleClass("fa fa-eye-slash");
+		if($(this).hasClass("open"))
+		{
+			$(".password").attr("type","text");
+		}
+		else
+		{
+			$(".password").attr("type","password");
+		}
+	})
+	
+})
